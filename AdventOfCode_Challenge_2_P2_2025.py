17c645b5be4b0e7ef28914354f2384d1f8327ff5
Joshua <jsferreirav2@gmail.com>
2025-12-07 16:25:01 +0000

    Completed Challenge 2 Part 2

---
+++ AdventOfCode_Challenge_2_P2_2025.py
@@ -0,0 +1,25 @@
+# Extract each range of codes from the input file
+with open("challenge2input.txt", "r") as file:
+    coderanges = file.read().split(",")
+
+#Initialize sum of invalid codes
+invalidsum = 0
+
+# Split each range into lower and upper bounds
+for coderange in coderanges:
+    split = coderange.index("-")
+    lowerbound = int(coderange[:split])
+    upperbound = int(coderange[split + 1 :])
+    # iterate through each identifier in the range
+    for identifier in range(lowerbound, upperbound + 1):
+        stringid = str(identifier)
+
+        # for each value from 1 to half the length of the string, check if the value repeated forms the whole string. If so, add to invalid sum.
+        for value in range(1, len(stringid)//2 + 1):
+            if stringid[:value] * (len(stringid) // value) == stringid:
+                invalidsum += identifier
+                # break from the loop to ensure each invalid identifier is only counted once
+                break
+
+# Print the sum of invalid codes
+print(invalidsum)
