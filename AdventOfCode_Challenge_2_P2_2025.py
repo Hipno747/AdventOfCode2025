5b3f2524e5baa99c6da86ca18c14a82469b8a60b
Joshua <jsferreirav2@gmail.com>
2025-12-07 19:09:26 +0000

    Completed Challenge 2 Part 2

--- AdventOfCode_Challenge_2_P2_2025.py
+++ AdventOfCode_Challenge_2_P2_2025.py
@@ -1,11 +1,11 @@
-# Extract each range of codes from the input file
+# extract each range of codes from the input file
 with open("challenge2input.txt", "r") as file:
     coderanges = file.read().split(",")
 
-#Initialize sum of invalid codes
+# initialise sum of invalid codes
 invalidsum = 0
 
-# Split each range into lower and upper bounds
+# split each range into lower and upper bounds
 for coderange in coderanges:
     split = coderange.index("-")
     lowerbound = int(coderange[:split])
@@ -21,5 +21,6 @@ for coderange in coderanges:
                 # break from the loop to ensure each invalid identifier is only counted once
                 break
 
-# Print the sum of invalid codes
+# output the sum of invalid codes
+
 print(invalidsum)
